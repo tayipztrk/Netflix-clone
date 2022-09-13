8f41bff8a947490c2b7bb3062958f0566cb64548
tayipztrk <tayipztrk@icloud.com>
2022-09-13 10:35:35 +0300

    login added

--- backend/netflix.Repository/Migrations/20220912202115_User_Program_Added.Designer.cs
+++ backend/netflix.Repository/Migrations/20220912202115_User_Program_Added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using netflix.Repository.Context;
 
@@ -11,9 +12,10 @@ using netflix.Repository.Context;
 namespace netflix.Repository.Migrations
 {
     [DbContext(typeof(netflixDbContext))]
-    partial class netflixDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220912202115_User_Program_Added")]
+    partial class User_Program_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/netflix.Repository/Migrations/20220912202115_User_Program_Added.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace netflix.Repository.Migrations
+{
+    public partial class User_Program_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPrograms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ProgramId = table.Column<int>(type: "int", nullable: false),
+                    WatchDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WatchTime = table.Column<int>(type: "int", nullable: false),
+                    EpisodeNumber = table.Column<int>(type: "int", nullable: false),
+                    Point = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPrograms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPrograms_Programs_ProgramId",
+                        column: x => x.ProgramId,
+                        principalTable: "Programs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserPrograms_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPrograms_ProgramId",
+                table: "UserPrograms",
+                column: "ProgramId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPrograms_UserId",
+                table: "UserPrograms",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPrograms");
+        }
+    }
+}
