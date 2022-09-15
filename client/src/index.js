6754fb4a1eff1cf094ec2cae46d6f38e89ac41c3
tayipztrk <tayipztrk@icloud.com>
2022-09-15 15:26:01 +0300

    I think its finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,111 +1,116 @@
 const electron = require("electron");
 
-const { app, BrowserWindow,ipcMain } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
-const electronReload = require('electron-reload')
+const electronReload = require("electron-reload");
 const fetch = require("electron-fetch").default;
 
-const path=require('path')
-const url=require('url')
+const path = require("path");
+const url = require("url");
 
 let user;
 let programs;
-let win 
-let login
-let signup
+let win;
+let login;
+let signup;
 
 function createWindows() {
-    
-    win =new BrowserWindow({width:1100,height:800,webPreferences: {
+  win = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-  }})
+    },
+  });
 
-    win.loadURL(`file://${__dirname}/login.html`)
-  
+  win.loadURL(`file://${__dirname}/login.html`);
 }
 ipcMain.on("userlogin", (event, data) => {
-  console.log(data)
-      fetch('https://localhost:7179/User/Login', {
-        method: "POST",
-        body: data,
-        headers: { 'Content-Type': 'application/json' },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("succes:",data);
-          user = data;
-          win.loadURL(`file://${__dirname}/index.html`)
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+  fetch("https://localhost:7179/User/Login", {
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("succes:", data);
+      user = data;
+      win.loadURL(`file://${__dirname}/index.html`);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 ipcMain.on("usersignup", (event, data) => {
-  console.log(data)
-      fetch('https://localhost:7179/User/Register', {
-        method: "POST",
-        body: data,
-        headers: { 'Content-Type': 'application/json' },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("succes:",data);
-          user = data;
-          win.loadURL(`file://${__dirname}/chooseContent.html`)
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+  console.log(data);
+  fetch("https://localhost:7179/User/Register", {
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("succes:", data);
+      user = data;
+      win.loadURL(`file://${__dirname}/chooseContent.html`);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 ipcMain.on("getPrograms", (event) => {
-      fetch('https://localhost:7179/User/Program')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        win.webContents.send('store-programs', data);
-      })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+  fetch("https://localhost:7179/User/Program")
+    .then((response) => response.json())
+    .then((data) => {
+      dataList = {
+        programs: data,
+        userInterests: user.interests,
+      };
+      console.log(data)
+      win.webContents.send("store-programs", dataList);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 ipcMain.on("userInterest", (event, data) => {
-      dataLast = {
-        interestId: data,
-        userId: user.user.id
-      }
-      fetch('https://localhost:7179/User/Interest', {
-        method: "POST",
-        body: JSON.stringify(dataLast),
-        headers: { 'Content-Type': 'application/json' },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("succes:",data);
-          win.loadURL(`file://${__dirname}/index.html`)
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+  dataLast = {
+    interestId: data,
+    userId: user.id,
+  };
+  fetch("https://localhost:7179/User/Interest", {
+    method: "POST",
+    body: JSON.stringify(dataLast),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      user = data;
+      console.log("succes:", data);
+      win.loadURL(`file://${__dirname}/index.html`);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 ipcMain.on("watch", (event, data) => {
   dataList = {
-    programId: data,
-    userId: user.user.id
-  }
-  fetch('https://localhost:7179/User/Watch', {
+    userId: user.user.id,
+    programId: parseInt(data),
+  };
+  fetch("https://localhost:7179/User/Watch", {
     method: "POST",
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dataList),
+    headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log("succes:",data);
-      win.loadURL(`file://${__dirname}/index.html`)
+      console.log("succes:", data);
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -113,15 +118,14 @@ ipcMain.on("watch", (event, data) => {
 });
 
 ipcMain.on("authenticated", async (event) => {
-        // win.show()
-        // login.hide()
-        // signup.hide()
-        win.loadURL(`file://${__dirname}/index.html`)
-  })
+  // win.show()
+  // login.hide()
+  // signup.hide()
+  win.loadURL(`file://${__dirname}/index.html`);
+});
 
-  ipcMain.on("unauthenticated", (event) => {
+ipcMain.on("unauthenticated", (event) => {
+  win.loadURL(`file://${__dirname}/signIn.html`);
+});
 
-    win.loadURL(`file://${__dirname}/signIn.html`)
-  })
-
-app.on('ready',createWindows)
+app.on("ready", createWindows);
